3d617fa348eaaf7763bf554e3372bf9d34c89e57
Isaac Wilkinson <isaacwilkinson@Isaacs-MBP.attlocal.net>
2023-12-02 12:24:15 -0500

    changed proj from node.js to ts and Bun

--- day01/day01.ts
+++ day01/day01.ts
@@ -1,43 +1,30 @@
-// The elves calibration document is jumbled
-// in each line the calibration value you need to
-// get is the first number appended to the last number
-// ex: a1b2c3d should yeild 13
-// Those with only 1 number should be that number repeated
-// ex: treb7uchet should yield 77
-// find the sum of all the calibration values
-
-import { read, readFileSync} from "node:fs";
-
 const integers = "0123456789";
 const ints = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const intsWritten = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 const intPairsWritten = ["twone", "sevenine", "oneight", "threeight", "nineight", "fiveight", "eighthree", "eightwo"];
 const intPairs = ["21", "79", "18", "38", "98", "58", "83", "82"];
 
+const file = Bun.file("day01.txt");
+const text = await file.text();
+const fileInput = text.replace(/\r/g, "")
+  .trim()
+  .split("\n");  
+
 function question1() {
-  let input = readFile();
-  getCalibrationValues(input);
+  getCalibrationValues(fileInput);
 }
 
 function question2() {
-  let input = readFile();
-  input = cleanInput(input);
-  getCalibrationValues(input);
+  let cleanedInput = cleanInput(fileInput);
+  getCalibrationValues(cleanedInput);
   
 }
 
-function readFile() {
-  return readFileSync("day01.txt", { encoding: "utf-8" })
-  .replace(/\r/g, "")
-  .trim()
-  .split("\n");
-}
-
-function getNumbersFromString(myString) {
+function getNumbersFromString(myString: string) {
   return [...myString].reduce((acc, cur) => ((integers.includes(cur)) ? acc + cur : acc), "");
 }
 
-function cleanInput(input){
+function cleanInput(input: string[]){
   let arr = [];
   for (let line of input) {
     let tempLine = line;
@@ -53,7 +40,7 @@ function cleanInput(input){
   return arr;
 }
 
-function getCalibrationValues(inputString) {
+function getCalibrationValues(inputString: string[]) {
   let total = 0;
   for (let value of inputString) {
     let numbers = getNumbersFromString(value);
